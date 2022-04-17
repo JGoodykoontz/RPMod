86838c929a6609d7f615a4e91f58c59dd0702433
JGoodykoontz <jgoodyko@ucsc.edu>
2022-04-17 16:32:27 -0700

    Added comment in main

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+//
+//  Created by: Jacquelyn Goodykoontz
+//  Title: Harpoon
+//  Date Completed: 04/17/2022
+//  Time Spent: > 10 hours
+//
+//  Points:
+//      - 60 points for aesthetic/theme redesign. All assets were made by me
+//      - 30 points for simultaneous two player
+//      - 10 points for parallax scrolling
+//      - 10 points for new title screen
+//
 let config = {
     type: Phaser.CANVAS, 
     width: 640, 
